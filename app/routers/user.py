9fd10b946e5887aff1ede60778e5b79457578d03
tojtt <hoangto@berkeley.edu>
2023-09-24 11:45:46 -0700

    add prefixes to APIRouter function

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,9 +3,11 @@ from sqlalchemy.orm import Session
 from .. import models, schema, utils
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users"
+)
 
-@router.post("/users", status_code = status.HTTP_201_CREATED, response_model=schema.UserOut)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schema.UserOut)
 def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
     #hash the password
     hashed_password = utils.hash(user.password)
@@ -16,7 +18,7 @@ def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get("/users/{id}", response_model=schema.UserOut )
+@router.get("/{id}", response_model=schema.UserOut )
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
