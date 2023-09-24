e9c7ccdeeb37c28f8a29d014de05c545c55c5455
tojtt <hoangto@berkeley.edu>
2023-09-24 11:08:59 -0700

    update

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
+
 from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schema
+from . import models, schema, utils
 from .database import engine, get_db
+
 
 models.Base.metadata.create_all(bind=engine) #only check if the table with the specific name exists or not
 
@@ -108,5 +110,20 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+@app.post("/users", status_code = status.HTTP_201_CREATED, response_model=schema.UserOut)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    #hash the password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password 
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
-
+@app.get("/users/{id}", response_model=schema.UserOut )
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'post with id:{id} not found')
+    return user
