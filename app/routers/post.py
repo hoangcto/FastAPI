66496c57f28eaa12c0ea0d06f88eabd49d498fa6
tojtt <hoangto@berkeley.edu>
2023-09-28 15:26:38 -0700

    Sqlalchemy Relationships: including users info

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,16 +10,14 @@ router = APIRouter(
 )
 
 # getting all posts
-@router.get("/", response_model=List[schema.Response])
+@router.get("/", response_model=List[schema.Post])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)
     posts = db.query(models.Post).all()
     return posts
 
 # creating a post
-@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schema.Response)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schema.Post)
 def create_posts(post: schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user.email)
     new_post = models.Post(owner_id = current_user.id,
         **post.model_dump())
     db.add(new_post)
@@ -28,23 +26,23 @@ def create_posts(post: schema.PostCreate, db: Session = Depends(get_db), current
     return new_post
 
 # getting individual post
-@router.get("/{id}", response_model=schema.Response)
+@router.get("/{id}", response_model=schema.Post)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'post with id:{id} not found')
     return post
 
-@router.put("/{id}", response_model=schema.Response)
+@router.put("/{id}", response_model=schema.Post)
 def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post_to_update = post_query.first()
     if post_to_update == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'post with id:{id} does not exist')
+    
     if post_to_update.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail = f'Not authorized to perform request action')
+    
     post_query.update( post.model_dump(), synchronize_session=False)
     db.commit()
 
@@ -52,7 +50,6 @@ def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db),
 
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user)
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post == None:
@@ -60,6 +57,7 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     
     if post.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail = f'Not authorized to perform request action')
+    
     post_query.delete(synchronize_session= False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/schema.py
+++ app/schema.py
@@ -10,13 +10,6 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
-class Response(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-    
-    class Config:
-        orm_mode = True
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -33,6 +26,16 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+    
+    class Config:
+        orm_mode = True
+
 
 class Token(BaseModel):
     access_token: str
